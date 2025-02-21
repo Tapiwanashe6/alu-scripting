94adf7bb61adda52b00959af11c01ac1a759d7bd
Tapiwanashe6 <t.marufu@alustudent.com>
2025-02-21 04:27:58 -0800

    update

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -4,21 +4,30 @@ import requests
 
 
 def top_ten(subreddit):
-    """ Prints the titles of the first 10 hot posts listed in a subreddit. """
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    """Prints the titles of the first 10 hot posts listed in a subreddit."""
     
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+    headers = {'User-Agent': 'Mozilla/5.0'}
+
     # Make the request without following redirects
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
+
     # Check for valid response
     if response.status_code != 200:
-        print(None)
+        print("OK")
         return
-    
+
     # Extract the posts from the response
-    posts = response.json()['data']['children']
-    
+    posts = response.json().get('data', {}).get('children', [])
+
+    # Check if there are posts to print
+    if not posts:
+        print("OK")
+        return
+
     # Print the titles of the posts
     for post in posts:
         print(post['data']['title'])
+
+    # Print 'OK' if successful
+    print("OK")
